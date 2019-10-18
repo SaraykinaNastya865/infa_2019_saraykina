ea41a114ffefb5a44bc307fc98315ac7583e59dd
SaraykinaNastya865 <saraikina.aa@phystech.edu>
2019-10-19 01:57:57 +0300

    Домашняя работа с изменениями. 6.1 и 6.2

--- lab3/panda 6.1.py
+++ lab3/panda 6.1.py
@@ -2,7 +2,7 @@ from graph import *
 
 brushColor ("#F4A460")
 penColor ("#F4A460")
-rectangle (0, 0, 1000, 600)
+rectangle (0, 0, 1000, 700)
 
 def myline (x, y, sh_x, sh_y, d):
     p1=[]
@@ -33,20 +33,20 @@ def list(r,x,y,sh_x,sh_y):
         n -= 5
         r0 -= 1
 
-def ear1(x,y):
-    p0 = myline(x,y,2,4,1)
+def ear1(x,y,k):
+    p0 = myline(x,y,2*k,4*k,1*k)
     tmp = p0[-1]
     x = tmp[0]
     y = tmp[1]
-    p1 = myline(x,y,6,-4,1)
+    p1 = myline(x,y,6*k,-4*k,1*k)
     tmp = p1[-1]
     x = tmp[0]
     y = tmp[1]
-    p2 = myline(x,y,-3,-3,-1)
+    p2 = myline(x,y,-3*k,-3*k,-1*k)
     tmp = p2[-1]
     x = tmp[0]
     y = tmp[1]
-    p3 = myline(x,y,-3,1,-0.3)
+    p3 = myline(x,y,-3*k,1*k,-0.3*k)
     tmp = p3[-1]
     x = tmp[0]
     y = tmp[1]
@@ -58,20 +58,20 @@ def ear1(x,y):
     p4.append((x1,y1))
     polygon(p0+p1+p2+p3+p4)
 
-def ear2(x,y):
-    p0 = myline(x,y,-3,0.1,-1)
+def ear2(x,y,k):
+    p0 = myline(x,y,-3*k,0.1*k,-1*k)
     tmp = p0[-1]
     x = tmp[0]
     y = tmp[1]
-    p1 = myline(x,y,5,8,1)
+    p1 = myline(x,y,5*k,8*k,1*k)
     tmp = p1[-1]
     x = tmp[0]
     y = tmp[1]
-    p2 = myline(x,y,2,-0.2,1)
+    p2 = myline(x,y,2*k,-0.2*k,1*k)
     tmp = p2[-1]
     x = tmp[0]
     y = tmp[1]
-    p3 = myline(x,y,-1,-3,-0.3)
+    p3 = myline(x,y,-1*k,-3*k,-0.3*k)
     tmp = p3[-1]
     x = tmp[0]
     y = tmp[1]
@@ -106,14 +106,17 @@ def Tree(x1,y1,x2,y2,):
     branch (x2+3,y2+8,width/3,high/10,1) #right_down branch
     branch (x1-3,y2+high/2,-width/3,high/10,1)
     points1 = [(x1 + width/2, y2 - 10), (x1 - width/6, y2 - 15),
-              (x1 + width/2, y2 - 10 - high*2/3), (x1 + 7*width/6, y2 - 5 - high*2/3),
-              (x1 + width/2, y2 - 10)]
+               (x1 + width/2, y2 - 10 - high*2/3),
+               (x1 + 7*width/6, y2 - 5 - high*2/3),
+               (x1 + width/2, y2 - 10)]
     polygon (points1)
     branch (x1-width/6-5,y2-20,-width/2,high/9,1) #left_up branch
     branch (x1+7*width/6+5,y2 - 10 - high*2/3,width/2,high/9,1)
-    points2 = [(x2, y2 - 15 - high*2/3), (x2 - width/2, y2 - 20 - high*2/3),
-              (x2 + 10, y2 - 20 - high*5/3), (x2 + 10 + width/2, y2 - 15 - high*5/3),
-              (x2, y2 - 15 - high*2/3)]
+    points2 = [(x2, y2 - 15 - high*2/3),
+               (x2 - width/2, y2 - 20 - high*2/3),
+               (x2 + 10, y2 - 20 - high*5/3),
+               (x2 + 10 + width/2, y2 - 15 - high*5/3),
+               (x2, y2 - 15 - high*2/3)]
     polygon (points2)
 
 def ellipse (a,b,x0,y0):
@@ -122,10 +125,10 @@ def ellipse (a,b,x0,y0):
             if (x**2/a**2+y**2/b**2<=1):
                 point(x+x0,y+y0)
 
-def head (x,y):
+def head (x,y,k):
     brushColor ("#FFFFFF")
     penColor ("#FFFFFF")
-    p1 = myline(x,y,15,8,5)
+    p1 = myline(x,y,k*15,k*8,k*5)
     p0 = []
     p2 = []
     p3 = []
@@ -135,25 +138,20 @@ def head (x,y):
     y0 = tmp0[1]
     x2 = tmp2[0]
     y2 = tmp2[1]
-    p0.append((x0-10,y0-65))
-    p0.append((x0-9,y0-15))
+    p0.append((x0-k*10,y0-k*65))
+    p0.append((x0-k*9,y0-k*15))
     p0.append(tmp0)
     p2.append(tmp2)
-    p2.append((x2+15,y2-15))
-    p3.append((x2+17,y2-17))
-    p3.append((x2+20,y0-75))
-    p4 = myline(x2+20,y0-75,-8,-13,-2)
+    p2.append((x2+k*15,y2-k*15))
+    p3.append((x2+k*17,y2-k*17))
+    p3.append((x2+k*20,y0-k*75))
+    p4 = myline(x2+k*20,y0-k*75,-8*k,-13*k,-2*k)
     tmp4 = p4[-1]
     x4 = tmp4[0]
     y4 = tmp4[1]
-    p5 = myline(x4,y4,-12,-1,-3)
-    p5.append((x0-10,y0-65))
+    p5 = myline(x4,y4,-12*k,-1*k,-3*k)
+    p5.append((x0-k*10,y0-k*65))
     polygon(p0+p1+p2+p3+p4+p5)
-    brushColor ("#000000")
-    penColor ("#000000")
-    circle(x4-20,y2-25,15)
-    ellipse(10,15,378,358)
-    ellipse(12,8,384,400)
 
 def leg1(x,y,k):
     penSize(3)
@@ -227,29 +225,36 @@ def leg3 (x,y,k):
     polygon(p0+p1+p2+p3)
 
 def panda():
-        penColor("#FFFFFF")
-        ellipse(92,50,460,370)
-        leg1(406,368,1)
-        leg2(480,315,1)
-        leg3(500,495,0.9)
-        head (380, 400)
-        penSize(4)
-        ear1(360,305)
-        ear2(463,285)
+    #big panda
+    penColor("#FFFFFF")
+    ellipse(101,55,468,405)    #body
+    leg1(411,408,1.1)
+    leg2(488,355,1.1)
+    leg3(510,545,1)
+    head (380,440,1.1)
+    brushColor ("#000000")
+    penColor ("#000000")
+    circle(415,405,16)        #eye
+    ellipse(11,16,378,398)    #eye
+    ellipse(13,9,384,440)     #nose
+    penSize(4)
+    ear1(358,338,1.1)
+    ear2(470,317,1.1)
 
-windowSize(700,600)
-canvasSize(700,600)
+windowSize(800,650)
+canvasSize(800,650)
 penSize(2)
-Tree(320, 470, 345, 370)
 Tree(200, 480, 205, 425)
+Tree(320, 470, 345, 370)
+#tree leaves
 for i in range (5):
-    list(5,141+20*i,153+i+i+i,-0.2,0.6)
-    list(5,521-20*i,90+i+i+i,0.2,0.6)
-    ellipse(3,10,160+7*i,330-i)
+    ellipse(3,10,160+7*i,330-i)        #tree1
     ellipse(3,10,220+7*i,300+i)
+    list(5,141+20*i,153+i+i+i,-0.2,0.6)#tree2
+    list(5,521-20*i,90+i+i+i,0.2,0.6)
 for i in range (3):
-    list(5,206+23*i,243+i+i+i,-0.2,0.6)
-    list(5,406+23*i,203-i-i-i,0.2,0.6)
-    ellipse(3,10,175+7*i,390-i*10)
+    ellipse(3,10,175+7*i,390-i*10)     #tree1
     ellipse(3,10,230-7*i,375-i*12)
+    list(5,206+23*i,243+i+i+i,-0.2,0.6)#tree2
+    list(5,406+23*i,203-i-i-i,0.2,0.6)
 panda()
